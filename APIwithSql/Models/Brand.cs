2bca4b8eed0dcf098cfd049f01300aed682c4024
chriscalver <130410868+chriscalver@users.noreply.github.com>
2024-02-24 09:11:39 -0500

    Add project files.

---
+++ APIwithSql/Models/Brand.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace APIwithSql.Models
+{
+    public class Brand
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public int IsActive { get; set; }
+    }
+}
